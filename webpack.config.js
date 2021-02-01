d5508a764430cb395c0b17496790fad0b0f94e6d
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-01 19:57:12 +0300

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,62 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   module: {
     rules: [
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
-      { test: /\.(js)$/, use: "babel-loader" },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
       { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "images",
+        },
       },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
   output: {
-    filename: "bundle.js",
+    publicPath: "",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
