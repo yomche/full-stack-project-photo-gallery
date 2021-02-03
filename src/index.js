6ad5b45cbd515c9fb6968d82860d1cccf60fcd5f
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-03 13:09:41 +0300

    implementation of a photo slider on JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+import "./index.html";
 import "./style.scss";
+
+const listElements = document.querySelectorAll(".photo-section__element");
+const leftArrow = document.querySelector(".prev-arrow");
+const rightArrow = document.querySelector(".next-arrow");
+
+let numberOfClicks = 0;
+let numberOfVisiblePhotos = 4;
+
+const swipeToRight = () => {
+  if (numberOfClicks == -1) {
+    numberOfClicks++;
+  }
+  if (numberOfClicks < listElements.length - numberOfVisiblePhotos) {
+    listElements[numberOfClicks].style.display = "none";
+    numberOfClicks++;
+  }
+};
+
+const swipeToLeft = () => {
+  if (numberOfClicks == listElements.length - numberOfVisiblePhotos) {
+    numberOfClicks--;
+  }
+  if (numberOfClicks >= 0) {
+    listElements[numberOfClicks].style.display = "";
+    numberOfClicks--;
+  }
+};
+
+rightArrow.addEventListener("click", swipeToRight);
+leftArrow.addEventListener("click", swipeToLeft);
