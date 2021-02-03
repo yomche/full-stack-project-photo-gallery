376176db2ea49f6a9368ec53bad4e6b948f5184f
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-03 13:49:09 +0300

    change variable name

--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ const listElements = document.querySelectorAll(".photo-section__element");
 const leftArrow = document.querySelector(".prev-arrow");
 const rightArrow = document.querySelector(".next-arrow");
 
-let numberOfClicks = 0;
+let currentPhotoNumber = 0;
 let numberOfVisiblePhotos = 4;
 
 const swipeToRight = () => {
-  if (numberOfClicks == -1) {
-    numberOfClicks++;
+  if (currentPhotoNumber == -1) {
+    currentPhotoNumber++;
   }
-  if (numberOfClicks < listElements.length - numberOfVisiblePhotos) {
-    listElements[numberOfClicks].style.display = "none";
-    numberOfClicks++;
+  if (currentPhotoNumber < listElements.length - numberOfVisiblePhotos) {
+    listElements[currentPhotoNumber].style.display = "none";
+    currentPhotoNumber++;
   }
 };
 
 const swipeToLeft = () => {
-  if (numberOfClicks == listElements.length - numberOfVisiblePhotos) {
-    numberOfClicks--;
+  if (currentPhotoNumber == listElements.length - numberOfVisiblePhotos) {
+    currentPhotoNumber--;
   }
-  if (numberOfClicks >= 0) {
-    listElements[numberOfClicks].style.display = "";
-    numberOfClicks--;
+  if (currentPhotoNumber >= 0) {
+    listElements[currentPhotoNumber].style.display = "";
+    currentPhotoNumber--;
   }
 };
 
