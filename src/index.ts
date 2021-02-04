29b183fb1e19233522b7e0031ea5f3a0efbbe6b1
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-05 01:07:11 +0300

    format index.ts with eslint

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import "./style.scss";
-import "./index.html";
+import './style.scss';
+import './index.html';
 
-const slider = document.querySelector<HTMLElement>(".photo-section__slider");
-const leftArrow = document.querySelector<HTMLElement>(".prev-arrow");
-const rightArrow = document.querySelector<HTMLElement>(".next-arrow");
-const sliderElements = document.querySelectorAll<HTMLElement>(".photo-section__element");
+const slider = document.querySelector<HTMLElement>('.photo-section__slider');
+const leftArrow = document.querySelector<HTMLElement>('.prev-arrow');
+const rightArrow = document.querySelector<HTMLElement>('.next-arrow');
+const sliderElements = document.querySelectorAll<HTMLElement>('.photo-section__element');
 
-let photoWidth:number = 300;
-let numberOfSlidingPhoto:number = 1;
-let position:number = 0;
-const numberOfVisiblePhotos: number = 4;
+const photoWidth = 300;
+const numberOfSlidingPhoto = 1;
+let position = 0;
+const numberOfVisiblePhotos = 4;
 
 const swipeToLeft = (): void => {
   position += photoWidth * numberOfSlidingPhoto;
   position = Math.min(position, 0);
-  slider.style.transform = "translateX(" +  position + "px)";
+  slider.style.transform = 'translateX(' + position + 'px)';
 };
 
 const swipeToRight = (): void => {
   position -= photoWidth * numberOfSlidingPhoto;
   position = Math.max(position, -photoWidth * (sliderElements.length - numberOfVisiblePhotos));
-  slider.style.transform = "translateX(" +  position + "px)";
+  slider.style.transform = 'translateX(' + position + 'px)';
 };
 
-leftArrow.addEventListener("click", swipeToLeft);
-rightArrow.addEventListener("click", swipeToRight);
+leftArrow.addEventListener('click', swipeToLeft);
+rightArrow.addEventListener('click', swipeToRight);
