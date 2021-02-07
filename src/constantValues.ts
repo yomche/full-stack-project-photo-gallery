a5527d4d7d14ebf521a71a43f116504906e3e652
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-07 23:17:39 +0300

    move constant variables into a separate file

---
+++ src/constantValues.ts
@@ -0,0 +1,7 @@
+export const slider = document.querySelector<HTMLElement>('.photo-section__slider');
+export const leftArrow = document.querySelector<HTMLElement>('.prev-arrow');
+export const rightArrow = document.querySelector<HTMLElement>('.next-arrow');
+
+export const photoWidth = 300;
+export const numberOfSlidingPhoto = 1;
+export const numberOfVisiblePhotos = 4;
