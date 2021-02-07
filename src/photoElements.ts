2b131dbcf0bead811b00c64e6581ac4d3cc21396
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-02-07 23:28:09 +0300

    add an array with photos for gallery

---
+++ src/photoElements.ts
@@ -0,0 +1,50 @@
+export const photoElements = [
+  {
+    alternativeName: 'Gallery photo 1',
+    photoSource: 'images/pict1.png',
+  },
+  {
+    alternativeName: 'Gallery photo 2',
+    photoSource: 'images/pict2.png',
+  },
+  {
+    alternativeName: 'Gallery photo 3',
+    photoSource: 'images/pict3.png',
+  },
+  {
+    alternativeName: 'Gallery photo 4',
+    photoSource: 'images/pict4.png',
+  },
+  {
+    alternativeName: 'Gallery photo 5',
+    photoSource: 'images/pict5.png',
+  },
+  {
+    alternativeName: 'Gallery photo 6',
+    photoSource: 'images/pict6.png',
+  },
+  {
+    alternativeName: 'Gallery photo 7',
+    photoSource: 'images/pict7.png',
+  },
+  {
+    alternativeName: 'Gallery photo 8',
+    photoSource: 'images/pict8.png',
+  },
+  {
+    alternativeName: 'Gallery photo 9',
+    photoSource: 'images/pict9.png',
+  },
+  {
+    alternativeName: 'Gallery photo 10',
+    photoSource: 'images/pict10.png',
+  },
+  {
+    alternativeName: 'Gallery photo 11',
+    photoSource: 'images/pict11.png',
+  },
+  {
+    alternativeName: 'Gallery photo 12',
+    photoSource: 'images/pict12.png',
+  },
+];
